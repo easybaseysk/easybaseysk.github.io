2e024b2432ab00c89585a2459d980ff055ae4ba9
ethanlau607 <ethanlau607@gmail.com>
2025-08-17 16:58:48 +0800

    update

--- js/textpage_script.js
+++ js/textpage_script.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.documentElement.style.setProperty(`--textColor`, localStorage.getItem('textColor'))
+
+    const homeIcon = document.querySelector('.home-icon');
+    if (homeIcon) {
+        homeIcon.addEventListener('click', () => {
+            window.location.href = '../index.html';
+        });
+    }
 });
